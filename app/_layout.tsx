3ba610a046765f0b80c2538ea15ad2139f832f4f
Matteo <matteo.courquin@gmail.com>
2025-07-09 14:12:31 +0200

    :bug: Try to add tailwind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import '../app/global.css';
 
 // import { useColorScheme } from '@/hooks/useColorScheme';
 
